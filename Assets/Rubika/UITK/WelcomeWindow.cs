f6ca4852043fa16b76779ba5130eb37cb88413cb
Remi Slysz <remi@unity3d.com>
2022-01-19 09:47:34 +0100

    Update UITK

--- Assets/Rubika/UITK/WelcomeWindow.cs
+++ Assets/Rubika/UITK/WelcomeWindow.cs
@@ -31,17 +31,48 @@ public class WelcomeWindow : EditorWindow
         VisualElement treeFromUXML = visualTree.Instantiate();
         root.Add(treeFromUXML);
 
-        // Link elements
+        // Link elements of UXML
         var button = root.Q<Button>("SendToLog");
         @switch = root.Q<Toggle>("Switch");
         sentence = root.Q<TextField>("Sentence");
-
+        
+        // Event handling
         button.clicked += SendToLog;
+
+        // Add field with reactions
+        Toggle reactSwitch = new Toggle("Show next line");
+        root.Add(reactSwitch);
+        Label hiddenText = new Label("Hidden text");
+        root.Add(hiddenText);
+
+        // Event for value changed
+        reactSwitch.RegisterValueChangedCallback(OnReactSwitchValueChange(hiddenText));
+
+        // Init reatcions
+        hiddenText.style.display = DisplayStyle.None; //hidden by default
+        reactSwitch.SetValueWithoutNotify(false);
     }
 
     void SendToLog()
     {
         if (@switch.value)
             UnityEngine.Debug.Log(sentence.text);
+    }
+
+    EventCallback<ChangeEvent<bool>> OnReactSwitchValueChange(Label hiddableLabel)
+    {
+        return (ChangeEvent<bool> evt) =>
+        {
+            if (evt.newValue)
+            {
+                //show
+                hiddableLabel.style.display = DisplayStyle.Flex;
+            }
+            else
+            {
+                //hide
+                hiddableLabel.style.display = DisplayStyle.None;
+            }
+        };
     }
 }
